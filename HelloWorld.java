0a799dd4c55e8bdc54b44284f5b934f01478b5bc
Hamna <hamnazulfiqarch@gmail.com>
2024-09-24 10:53:18 -0500

    added how many years i been to beloit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
-public class HelloWorld {
-
-	public static void main(String[] args){
-	  System.out.printlin("hello,world!")
-	}
-}
+public class HelloWorld { 
+ public static void main(String[] args) { 
+     System.out.println("Hello, World. I have been at beloit for 3 years."); 
+} 
+} 
